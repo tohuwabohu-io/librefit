abc152001bb4d9abc864e9c94281797e57fc4b5e
Stefan Poindl <stefan@tohuwabohu.io>
2023-02-24 21:45:04 +0100

    round bmr and tdee to next integer value

--- librefit-service/src/main/kotlin/io/tohuwabohu/calc/Tdee.kt
+++ librefit-service/src/main/kotlin/io/tohuwabohu/calc/Tdee.kt
@@ -1,16 +1,18 @@
 package io.tohuwabohu.calc
 
+import kotlin.math.round
+
 /**
  * Calculation of basic metabolic rate (BMR) and total daily energy expenditure (TDEE) based on the Harris-Benedict
  * formula.
  */
 data class Tdee (val age: Number, val sex: String, val weight: Number, val height: Number, val activityLevel: Number) {
     val bmr = if (sex == "m") {
-        66 + (13.7 * weight.toFloat()) + (5 * height.toFloat()) - (6.8 * age.toFloat())
+        round(66 + (13.7 * weight.toFloat()) + (5 * height.toFloat()) - (6.8 * age.toFloat()))
     } else {
-        655 + (9.6 * weight.toFloat()) + (1.8 * height.toFloat()) - (4.7 * age.toFloat())
+        round(655 + (9.6 * weight.toFloat()) + (1.8 * height.toFloat()) - (4.7 * age.toFloat()))
     }
 
-    val tdee = activityLevel.toFloat() * bmr
+    val tdee = round(activityLevel.toFloat() * bmr)
 }
 
