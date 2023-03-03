db00744b710135cec97050360235c8f40385b714
Stefan Poindl <stefan@tohuwabohu.io>
2023-03-04 00:27:45 +0100

    fix crypt called twice on test data

--- librefit-service/src/main/resources/db/libre_user.sql
+++ librefit-service/src/main/resources/db/libre_user.sql
@@ -26,9 +26,9 @@ alter function encrypt_pwd() owner to librefit;
 
 create trigger password_trigger before insert on libre_user for each row execute procedure encrypt_pwd();
 
-insert into libre_user (id, email, name, password, registered) values (1, 'test1@test.dev', 'test1', crypt('test1', gen_salt('bf')), '2023-03-01');
-insert into libre_user (id, email, name, password, registered) values (2, 'test2@test.dev', 'Johnny Cool', crypt('test2', gen_salt('bf')), '2023-03-01');
-insert into libre_user (id, email, name, password, registered) values (3, 'test3@test.dev', 'Hugh Jazz', crypt('test3', gen_salt('bf')), '2023-03-01');
-insert into libre_user (id, email, name, password, registered) values (4, 'test4@test.dev', 'Donald Duck', crypt('test4', gen_salt('bf')), '2023-03-01');
-
+insert into libre_user (id, email, name, password, registered) values (1, 'test1@test.dev', 'test1', 'test1', '2023-03-01');
+insert into libre_user (id, email, name, password, registered) values (2, 'test2@test.dev', 'Johnny Cool', 'test2', '2023-03-01');
+insert into libre_user (id, email, name, password, registered) values (3, 'test3@test.dev', 'Hugh Jazz', 'test3', '2023-03-01');
+insert into libre_user (id, email, name, password, registered) values (4, 'test4@test.dev', 'Donald Duck', 'test4', '2023-03-01');
 insert into libre_user (id, email, name, password, registered) values (5, 'test5@test.dev', 'Gustav Gans', 'test5', '2023-03-01');
+insert into libre_user (id, email, name, password, registered) values (6, 'test6@test.dev', 'Gustav Gans', 'test6', '2023-03-01');
