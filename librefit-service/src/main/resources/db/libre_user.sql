b5bb1abfd6adcfb2b3c46eecd35b609752cf93d7
Stefan Poindl <stefan@tohuwabohu.io>
2023-03-03 10:55:07 +0100

    add test user logins

--- librefit-service/src/main/resources/db/libre_user.sql
+++ librefit-service/src/main/resources/db/libre_user.sql
@@ -11,3 +11,8 @@ create table libre_user
 
 alter table libre_user
     owner to librefit;
+
+insert into libre_user (id, email, name, password, registered) values (1, 'test1@test.dev', 'test1', 'test1', '2023-03-01');
+insert into libre_user (id, email, name, password, registered) values (2, 'test2@test.dev', 'Johnny Cool', 'test2', '2023-03-01');
+insert into libre_user (id, email, name, password, registered) values (3, 'test3@test.dev', 'Hugh Jazz', 'test3', '2023-03-01');
+insert into libre_user (id, email, name, password, registered) values (4, 'test4@test.dev', 'Donald Duck', 'test4', '2023-03-01');
