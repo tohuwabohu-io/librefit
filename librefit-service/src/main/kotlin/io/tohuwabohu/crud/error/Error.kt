06430cfa44884762c96f239551b924e933e03195
Stefan Poindl <stefan@tohuwabohu.io>
2023-03-12 15:12:09 +0100

    add logging in ValidationErrorMapper

--- librefit-service/src/main/kotlin/io/tohuwabohu/crud/error/Error.kt
+++ librefit-service/src/main/kotlin/io/tohuwabohu/crud/error/Error.kt
@@ -1,6 +1,7 @@
 package io.tohuwabohu.crud.error
 
 import io.tohuwabohu.crud.validation.ValidationError
+import org.jboss.logging.Logger
 import javax.ws.rs.core.Response
 import javax.ws.rs.ext.ExceptionMapper
 import javax.ws.rs.ext.Provider
@@ -17,7 +18,11 @@ data class ErrorResponse(val message: String)
 
 @Provider
 class ValidationErrorMapper : ExceptionMapper<ValidationError> {
+    private val log: Logger = Logger.getLogger(javaClass)
+
     override fun toResponse(exception: ValidationError): Response {
+        log.error("Validation failed.", exception)
+
         return Response.status(Response.Status.BAD_REQUEST).entity(ErrorResponse(exception.message)).build()
     }
 
