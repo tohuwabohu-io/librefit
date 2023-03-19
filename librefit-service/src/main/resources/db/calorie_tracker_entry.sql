70ae2554610bc77ca71ed6df486ec73cd4f77f92
Stefan Poindl <stefan@tohuwabohu.io>
2023-03-19 20:19:36 +0100

    add some calorie tracker test data

--- librefit-service/src/main/resources/db/calorie_tracker_entry.sql
+++ librefit-service/src/main/resources/db/calorie_tracker_entry.sql
@@ -1,4 +1,4 @@
-create table public.calorie_tracker_entry
+create table calorie_tracker_entry
 (
     id          bigint not null
         primary key,
@@ -10,6 +10,10 @@ create table public.calorie_tracker_entry
     category    varchar(255)
 );
 
-alter table public.calorie_tracker_entry
+alter table calorie_tracker_entry
     owner to librefit;
 
+insert into calorie_tracker_entry (id, added, amount, description, updated, user_id, category)
+    values (1, '2022-03-19', 300, null, null, 1, 'b'),
+           (2, '2022-03-19', 500, null, null, 1, 'l'),
+           (3, '2022-03-19', 400, null, null, 1, 'd');
