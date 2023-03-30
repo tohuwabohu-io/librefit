8b3487eae9e144eb321c6f41ed4cfc766679d609
Stefan Poindl <stefan@tohuwabohu.io>
2023-03-30 22:25:00 +0200

    move test data inserts to 99_testdata.sql

--- librefit-service/src/main/resources/db/01_libre_user.sql
+++ librefit-service/src/main/resources/db/01_libre_user.sql
@@ -12,15 +12,14 @@ alter function encrypt_pwd() owner to librefit;
 
 create table public.libre_user
 (
-    id         bigint       not null
-        primary key,
-    email      varchar(255) not null,
+    id         serial   primary key,
+    email      varchar(255) not null
+        constraint email_unique
+            unique,
     last_login timestamp,
     name       varchar(255),
-    password   varchar(255) not null
-        constraint pwd_unique
-            unique,
-    registered timestamp
+    password   varchar(255) not null,
+    registered timestamp default CURRENT_TIMESTAMP
 );
 
 alter table public.libre_user
@@ -31,10 +30,3 @@ create trigger password_trigger
     on public.libre_user
     for each row
 execute procedure public.encrypt_pwd();
-
-insert into libre_user (id, email, name, password, registered) values (1, 'test1@test.dev', 'test1', 'test1', '2023-03-01');
-insert into libre_user (id, email, name, password, registered) values (2, 'test2@test.dev', 'Johnny Cool', 'test2', '2023-03-01');
-insert into libre_user (id, email, name, password, registered) values (3, 'test3@test.dev', 'Hugh Jazz', 'test3', '2023-03-01');
-insert into libre_user (id, email, name, password, registered) values (4, 'test4@test.dev', 'Donald Duck', 'test4', '2023-03-01');
-insert into libre_user (id, email, name, password, registered) values (5, 'test5@test.dev', 'Gustav Gans', 'test5', '2023-03-01');
-insert into libre_user (id, email, name, password, registered) values (6, 'test6@test.dev', 'Darkwing Duck', 'test6', '2023-03-01');
--- librefit-service/src/main/resources/db/02_calorie_tracker_entry.sql
+++ librefit-service/src/main/resources/db/02_calorie_tracker_entry.sql
@@ -14,14 +14,3 @@ create table public.calorie_tracker_entry
 
 alter table public.calorie_tracker_entry
     owner to librefit;
-
-insert into calorie_tracker_entry (added, user_id, id, amount, description, updated, category)
-    values ('2022-03-19', 1, 1, 240, null, 1, 'b'),
-           ('2022-03-19', 1, 2, 600, null, 1, 'l'),
-           ('2022-03-19', 1, 3, 400, null, 1, 'd'),
-           ('2022-03-19', 1, 4, 400, null, 1, 's'),
-           ('2022-03-20', 1, 1, 600, null, 1, 'b'),
-           ('2022-03-20', 1, 2, 750, null, 1, 'l'),
-           ('2022-03-20', 1, 3, 400, null, 1, 'd'),
-           ('2022-03-21', 1, 1, 900, null, 1, 'l'),
-           ('2022-03-21', 1, 2, 800, null, 1, 'd');
