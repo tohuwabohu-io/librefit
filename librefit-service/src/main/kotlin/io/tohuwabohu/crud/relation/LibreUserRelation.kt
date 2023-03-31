b405350082706002470007eb11dee29dff7fc7fb
Stefan Poindl <stefan@tohuwabohu.io>
2023-03-31 16:54:42 +0200

    add weak entity default constructor and convenience method

--- librefit-service/src/main/kotlin/io/tohuwabohu/crud/relation/LibreUserRelation.kt
+++ librefit-service/src/main/kotlin/io/tohuwabohu/crud/relation/LibreUserRelation.kt
@@ -1,28 +1,37 @@
 package io.tohuwabohu.crud.relation
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import io.quarkus.hibernate.reactive.panache.kotlin.PanacheEntityBase
 import java.io.Serializable
 import java.time.LocalDate
 import javax.persistence.*
 
-class LibreUserCompositeKey : Serializable {
-    var userId: Long = 0L
-    var added: LocalDate? = null
+class LibreUserCompositeKey(
+    var userId: Long = 0L,
+    var added: LocalDate = LocalDate.now(),
     var id: Long = 0L
-}
+): Serializable
 
 @IdClass(LibreUserCompositeKey::class)
 @MappedSuperclass
 abstract class LibreUserWeakEntity : PanacheEntityBase {
     @Id
-    @Column(name = "user_id", nullable = false)
+    @Column(nullable = false)
     var userId: Long = 0L
 
     @Id
-    @Column(nullable = false, columnDefinition = "date default current_timestamp")
-    var added: LocalDate? = null
+    @Column(nullable = false)
+    var added: LocalDate = LocalDate.now()
 
     @Id
     @Column(nullable = false)
     var id: Long = 0L
+
+    @Transient
+    @JsonIgnore
+    fun getPrimaryKey(): LibreUserCompositeKey {
+        return LibreUserCompositeKey(
+            userId, added, id
+        )
+    }
 }
