19b48bceb6b2c69ce352c7b629feeb0a9eba15b8
Stefan Poindl <stefan@tohuwabohu.io>
2023-03-31 21:08:44 +0200

    update goal and weight tracking entities for new schema

--- librefit-service/src/main/kotlin/io/tohuwabohu/crud/WeightTracking.kt
+++ librefit-service/src/main/kotlin/io/tohuwabohu/crud/WeightTracking.kt
@@ -1,25 +1,48 @@
 package io.tohuwabohu.crud
 
+import io.quarkus.hibernate.reactive.panache.common.runtime.ReactiveTransactional
 import io.quarkus.hibernate.reactive.panache.kotlin.PanacheRepository
 import io.tohuwabohu.crud.relation.LibreUserWeakEntity
+import org.hibernate.Hibernate
 import java.time.LocalDateTime
 import javax.enterprise.context.ApplicationScoped
 import javax.persistence.Entity
 
 @Entity
-class WeightTrackerEntry : LibreUserWeakEntity() {
-    var amount: Float? = null
-    lateinit var updated: LocalDateTime
+data class WeightTrackerEntry (
+    var amount: Float,
+    var updated: LocalDateTime
+) : LibreUserWeakEntity() {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        other as WeightTrackerEntry
+
+        return userId == other.userId
+    }
+
+    override fun hashCode(): Int = javaClass.hashCode()
+
+    @Override
+    override fun toString(): String {
+        return this::class.simpleName + "(userId = $userId , amount = $amount , updated = $updated , added = $added , id = $id )"
+    }
 }
 
 @ApplicationScoped
 class WeightTrackerRepository : PanacheRepository<WeightTrackerEntry> {
     fun listForUser(userId: Long) = find("userId", userId).list()
 
+    @ReactiveTransactional
     fun create(weightTrackerEntry: WeightTrackerEntry) = persist(weightTrackerEntry)
 
-    fun updateTrackingEntry(weightTrackerEntry: WeightTrackerEntry) = update(
-        "amount = ?1, updated = ?2 where id=?3",
-        weightTrackerEntry.amount!!, LocalDateTime.now(), weightTrackerEntry.id!!
-    )
+    @ReactiveTransactional
+    fun updateTrackingEntry(weightTrackerEntry: WeightTrackerEntry) {
+        val key = weightTrackerEntry.getPrimaryKey()
+
+        update(
+            "amount = ?1 where user_id = ?2 and added = ?3 and id = ?4",
+            weightTrackerEntry.amount, key.userId, key.added, key.id
+        )
+    }
 }
