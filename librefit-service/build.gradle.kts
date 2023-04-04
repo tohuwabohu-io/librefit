13d73bcc8882f8cb1b5a477c796a9bc0d45fe051
Stefan Poindl <stefan@tohuwabohu.io>
2023-04-04 08:28:07 +0200

    add hibernate-validator dependency

--- librefit-service/build.gradle.kts
+++ librefit-service/build.gradle.kts
@@ -16,6 +16,7 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
+    implementation("io.quarkus:quarkus-hibernate-validator")
     implementation("io.quarkus:quarkus-hibernate-reactive-panache-kotlin")
     implementation("io.quarkus:quarkus-reactive-pg-client")
     implementation("io.quarkus:quarkus-smallrye-openapi")
