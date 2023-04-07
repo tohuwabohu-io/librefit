b608788967ea9282f5e519d01ad2551f864173b1
Stefan Poindl <stefan@tohuwabohu.io>
2023-04-07 11:54:57 +0200

    add TrackerInput icons; parameterize unit display

--- librefit-web/src/lib/components/TrackerInput.svelte
+++ librefit-web/src/lib/components/TrackerInput.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
+	import Trash from '$lib/assets/icons/trash.svg?component';
+	import AddKcal from '$lib/assets/icons/hamburger-plus.svg?component';
+	import AddWeight from '$lib/assets/icons/plus.svg?component';
+	import Add from '$lib/assets/icons/plus.svg?component';
+	import Edit from '$lib/assets/icons/pencil.svg?component';
+	import Check from '$lib/assets/icons/check.svg?component';
+	import CancelDelete from '$lib/assets/icons/trash-off.svg?component';
+	import CancelEdit from '$lib/assets/icons/pencil-off.svg?component';
 
 	export let value, dateStr, id;
 	export let existing = false;
 	export let disabled = false;
 	export let categories;
 	export let category;
+	export let unit;
 
 	const dispatch = createEventDispatcher();
 
@@ -76,30 +85,60 @@
 
 <div class="flex gap-2">
 	<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
-		<div class="input-group-shim">kcal</div>
+		<div class="input-group-shim">{unit}</div>
 		<input type="number" placeholder="Amount..." bind:value {disabled} />
-		<select {disabled} bind:value={category}>
-			{#each categories as category}
-				<option value={category.value}>{category.label}</option>
-			{/each}
-		</select>
+		{#if categories}
+			<select {disabled} bind:value={category}>
+				{#each categories as category}
+					<option value={category.value}>{category.label}</option>
+				{/each}
+			</select>
+		{/if}
 	</div>
 	{#if existing}
 		{#if !editing}
 			<button class="btn-icon variant-filled-secondary" on:click|preventDefault={change('update')}>
-				E
+				<span>
+					<Edit/>
+				</span>
 			</button>
-			<button class="btn-icon variant-filled" on:click|preventDefault={change('delete')}> D</button>
+			<button class="btn-icon variant-filled" on:click|preventDefault={change('delete')}>
+				<span>
+					<Trash/>
+				</span>
+			</button>
 		{:else}
 			<button
 				class="btn-icon variant-ghost-primary"
 				on:click={changeAction === 'update' ? update : remove}
 			>
-				Y
+				<span>
+					<Check/>
+				</span>
 			</button>
-			<button class="btn-icon variant-ghost-error" on:click|preventDefault={discard}> N </button>
+			<button class="btn-icon variant-ghost-error" on:click|preventDefault={discard}>
+				<span>
+					{#if changeAction === 'update'}
+						<CancelEdit/>
+					{:else if changeAction === 'delete'}
+						<CancelDelete/>
+					{:else}
+						X
+					{/if}
+				</span>
+			</button>
 		{/if}
 	{:else}
-		<button class="btn-icon variant-filled-primary" on:click|preventDefault={add}> A </button>
+		<button class="btn-icon variant-filled-primary" on:click|preventDefault={add}>
+			<span>
+				{#if unit === 'kcal'}
+					<AddKcal/>
+				{:else if unit === 'kg'}
+					<AddWeight/>
+				{:else}
+					<Add/>
+				{/if}
+			</span>
+		</button>
 	{/if}
 </div>
