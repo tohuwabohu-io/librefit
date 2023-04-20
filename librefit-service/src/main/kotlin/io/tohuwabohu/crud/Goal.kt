d5387dcdbe3f2ba78fe05a735265174d4518e067
Stefan Poindl <stefan@tohuwabohu.io>
2023-04-20 21:02:19 +0200

    update goal

--- librefit-service/src/main/kotlin/io/tohuwabohu/crud/Goal.kt
+++ librefit-service/src/main/kotlin/io/tohuwabohu/crud/Goal.kt
@@ -2,11 +2,16 @@ package io.tohuwabohu.crud
 
 import io.quarkus.hibernate.reactive.panache.common.runtime.ReactiveTransactional
 import io.quarkus.hibernate.reactive.panache.kotlin.PanacheRepository
+import io.smallrye.mutiny.Uni
+import io.tohuwabohu.crud.error.UnmodifiedError
+import io.tohuwabohu.crud.relation.LibreUserRelatedRepository
 import io.tohuwabohu.crud.relation.LibreUserWeakEntity
 import org.hibernate.Hibernate
 import java.time.LocalDateTime
 import javax.enterprise.context.ApplicationScoped
 import javax.persistence.Entity
+import javax.persistence.EntityNotFoundException
+import javax.persistence.PreUpdate
 import javax.transaction.Transactional
 
 @Entity
@@ -15,7 +20,13 @@ data class Goal (
     var endAmount: Float,
     var startDate: LocalDateTime,
     var endDate: LocalDateTime,
+    var updated: LocalDateTime
 ) : LibreUserWeakEntity() {
+    @PreUpdate
+    fun setUpdatedFlag() {
+        updated = LocalDateTime.now()
+    }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
@@ -33,16 +44,20 @@ data class Goal (
 }
 
 @ApplicationScoped
-class GoalsRepository : PanacheRepository<Goal> {
+class GoalsRepository : LibreUserRelatedRepository<Goal>() {
     fun listByUser(userId: Number) = find("userId", userId).list()
     fun findLatestForUser(userId: Number) = find("userId", userId).list() //..minByOrNull { it.startDate }!!
 
     @ReactiveTransactional
-    fun create(goal: Goal) = persist(goal)
+    fun updateGoal(goal: Goal): Uni<Int> {
+        return findById(goal.getPrimaryKey()).onItem().ifNull()
+            .failWith(EntityNotFoundException()).onItem().ifNotNull().transformToUni{ entry ->
+                val key = entry.getPrimaryKey()
 
-    @ReactiveTransactional
-    fun updateGoal(goal: Goal) = update(
-        "startDate = ?1, endDate = ?2, startAmount = ?3, endAmount = ?4",
-        goal.startDate, goal.endDate, goal.startAmount!!, goal.endAmount!!
-    )
+                update(
+                    "startAmount = ?1, endAmount = ?2, startDate = ?3, endDate = ?4, updated = ?5 where user_id = ?6 and added = ?7 and id = ?8",
+                    goal.startAmount, goal.endAmount, goal.startDate, goal.endDate, goal.updated, key.userId, key.added, key.id
+                )
+            }.onItem().ifNull().failWith{ UnmodifiedError(goal.toString()) }
+    }
 }
