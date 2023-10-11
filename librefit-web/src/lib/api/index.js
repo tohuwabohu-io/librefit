b4c7680c861737dff8148f40282ce801bef99bd0
Stefan Poindl <stefan@tohuwabohu.io>
2023-10-11 21:16:46 +0200

    update api in web

--- librefit-web/src/lib/api/index.js
+++ librefit-web/src/lib/api/index.js
@@ -43,13 +43,13 @@ export const Category = {
 
 /**
  * @typedef {Object} CalorieTrackerEntry
- * @property [Number] userId
+ * @property {Number} [userId]
  * @property {LocalDate} added
- * @property [Number] id
+ * @property {Number} [id]
  * @property {Number} amount
  * @property {Category} category
- * @property [String] updated
- * @property [String] description
+ * @property {String} [updated]
+ * @property {String} [description]
  */
 
 /**
@@ -59,24 +59,24 @@ export const Category = {
 
 /**
  * @typedef {Object} Goal
- * @property [Number] userId
+ * @property {Number} [userId]
  * @property {LocalDate} added
- * @property [Number] id
+ * @property {Number} [id]
  * @property {Number} startAmount
  * @property {Number} endAmount
  * @property {LocalDate} startDate
  * @property {LocalDate} endDate
- * @property [String] updated
+ * @property {String} [updated]
  */
 
 /**
  * @typedef {Object} LibreUser
- * @property [Number] id
+ * @property {Number} [id]
  * @property {String} email
  * @property {String} password
- * @property [String] name
- * @property [String] registered
- * @property [String] lastLogin
+ * @property {String} [name]
+ * @property {String} [registered]
+ * @property {String} [lastLogin]
  */
 
 /**
@@ -88,19 +88,19 @@ export const Category = {
  * @property {Number} activityLevel
  * @property {Number} weeklyDifference
  * @property {CalculationGoal} calculationGoal
- * @property [Number] bmr
- * @property [Number] tdee
- * @property [Number] deficit
- * @property [Number] target
+ * @property {Number} [bmr]
+ * @property {Number} [tdee]
+ * @property {Number} [deficit]
+ * @property {Number} [target]
  */
 
 /**
  * @typedef {Object} WeightTrackerEntry
- * @property [Number] userId
+ * @property {Number} [userId]
  * @property {LocalDate} added
- * @property [Number] id
- * @property [Number] amount
- * @property [String] updated
+ * @property {Number} [id]
+ * @property {Number} [amount]
+ * @property {String} [updated]
  */
 
 export const api = {
@@ -108,113 +108,134 @@ export const api = {
 		path: '/goals/create',
 		/** @see Goal */
 		contentType: 'application/json',
-		method: 'post'
+		method: 'post',
+		guarded: true
 	},
 	deleteGoal: {
 		path: '/goals/delete/{date}/{id}',
 		contentType: 'text/plain',
-		method: 'delete'
+		method: 'delete',
+		guarded: true
 	},
 	findLastGoal: {
 		path: '/goals/last',
 		contentType: 'text/plain',
-		method: 'get'
+		method: 'get',
+		guarded: true
 	},
 	readGoal: {
 		path: '/goals/read/{date}/{id}',
 		contentType: 'text/plain',
-		method: 'get'
+		method: 'get',
+		guarded: true
 	},
 	updateGoal: {
 		path: '/goals/update',
 		/** @see Goal */
 		contentType: 'application/json',
-		method: 'put'
+		method: 'put',
+		guarded: true
 	},
 	getTdeeCalculateAgeSexWeightHeightActivityLevelDiffGain: {
-		path: '/tdee/calculate/{age}/{sex}/{weight}/{height}/{activityLevel}/{weeklyDifference}/{calculationGoal}',
+		path: '/tdee/calculate/{age}/{sex}/{weight}/{height}/{activityLevel}/{diff}/{gain}',
 		contentType: 'text/plain',
-		method: 'get'
+		method: 'get',
+		guarded: false
 	},
 	createCalorieTrackerEntry: {
 		path: '/tracker/calories/create',
 		/** @see CalorieTrackerEntry */
 		contentType: 'application/json',
-		method: 'post'
+		method: 'post',
+		guarded: true
 	},
 	deleteCalorieTrackerEntry: {
 		path: '/tracker/calories/delete/{date}/{id}',
 		contentType: 'text/plain',
-		method: 'delete'
+		method: 'delete',
+		guarded: true
 	},
 	listCalorieTrackerDates: {
 		path: '/tracker/calories/list/dates',
 		contentType: 'text/plain',
-		method: 'get'
+		method: 'get',
+		guarded: true
 	},
 	listCalorieTrackerEntriesForDate: {
 		path: '/tracker/calories/list/{date}',
 		contentType: 'text/plain',
-		method: 'get'
+		method: 'get',
+		guarded: true
 	},
 	readCalorieTrackerEntry: {
 		path: '/tracker/calories/read/{date}/{id}',
 		contentType: 'text/plain',
-		method: 'get'
+		method: 'get',
+		guarded: true
 	},
 	updateCalorieTrackerEntry: {
 		path: '/tracker/calories/update',
 		/** @see CalorieTrackerEntry */
 		contentType: 'application/json',
-		method: 'put'
+		method: 'put',
+		guarded: true
 	},
 	createWeightTrackerEntry: {
 		path: '/tracker/weight/create',
 		/** @see WeightTrackerEntry */
 		contentType: 'application/json',
-		method: 'post'
+		method: 'post',
+		guarded: true
 	},
 	deleteWeightTrackerEntry: {
 		path: '/tracker/weight/delete/{date}/{id}',
 		contentType: 'text/plain',
-		method: 'delete'
+		method: 'delete',
+		guarded: true
 	},
 	findLastWeightTrackerEntry: {
 		path: '/tracker/weight/last',
 		contentType: 'text/plain',
-		method: 'get'
+		method: 'get',
+		guarded: true
 	},
 	listWeightTrackerEntriesRange: {
 		path: '/tracker/weight/list/{dateFrom}/{dateTo}',
 		contentType: 'text/plain',
-		method: 'get'
+		method: 'get',
+		guarded: true
 	},
 	listWeightTrackerEntries: {
 		path: '/tracker/weight/list/{date}',
 		contentType: 'text/plain',
-		method: 'get'
+		method: 'get',
+		guarded: true
 	},
 	readWeightTrackerEntry: {
 		path: '/tracker/weight/read/{date}/{id}',
 		contentType: 'text/plain',
-		method: 'get'
+		method: 'get',
+		guarded: true
 	},
 	updateWeightTrackerEntry: {
 		path: '/tracker/weight/update',
 		/** @see WeightTrackerEntry */
 		contentType: 'application/json',
-		method: 'put'
+		method: 'put',
+		guarded: true
 	},
 	postUserLogin: {
 		path: '/user/login',
 		/** @see LibreUser */
 		contentType: 'application/json',
-		method: 'post'
+		method: 'post',
+		guarded: false
 	},
 	postUserRegister: {
 		path: '/user/register',
 		/** @see LibreUser */
 		contentType: 'application/json',
-		method: 'post'
+		method: 'post',
+		guarded: false
 	}
 };
