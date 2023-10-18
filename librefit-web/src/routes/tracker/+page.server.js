537522e04865bcc37b9b1e464e64ae66f7aec1e9
Stefan Poindl <stefan@tohuwabohu.io>
2023-10-18 14:37:45 +0200

    add blank entry for today's ct preload

--- librefit-web/src/routes/tracker/+page.server.js
+++ librefit-web/src/routes/tracker/+page.server.js
@@ -1,4 +1,4 @@
-import { api } from '$lib/api/index.js';
+import { api, Category } from '$lib/api/index.js';
 import { proxyFetch } from '$lib/api/util.js';
 import { getDateAsStr } from '$lib/util.js';
 
@@ -14,14 +14,24 @@ export const load = async ({ fetch, cookies }) => {
 		date: getDateAsStr(new Date()) // today
 	});
 
-	if (
-		ctDateResponse.status === 200 &&
-		(listCtForDateResponse.status === 200 || listCtForDateResponse.status === 404) // 'not found' means no entry created today
-	) {
+	if (ctDateResponse.status === 200 && listCtForDateResponse.status === 200) {
 		const todayStr = getDateAsStr(new Date());
 
 		/** @type {Array<String>} */
 		const availableDates = await ctDateResponse.json();
+
+		/** @type {Array<CalorieTrackerEntry>} */
+		const ctList = await listCtForDateResponse.json();
+
+		// add a blank entry for new input
+		/** @type {CalorieTrackerEntry} */
+		const blankEntry = {
+			added: todayStr,
+			amount: 0,
+			category: Category.Unset
+		};
+
+		ctList.unshift(blankEntry);
 
 		if (availableDates.indexOf(todayStr) < 0) {
 			availableDates.unshift(todayStr);
@@ -29,8 +39,7 @@ export const load = async ({ fetch, cookies }) => {
 
 		return {
 			availableDates: availableDates,
-			entryToday:
-				listCtForDateResponse.status === 200 ? await listCtForDateResponse.json() : undefined
+			entryToday: ctList
 		};
 	} else {
 		return { error: 'An error has occured. Please try again later.' };
