e2795ed936967d3756990d1d05cee788bcf13db2
Stefan Poindl <stefan@tohuwabohu.io>
2023-11-04 22:37:24 +0100

    implement calorie tracker crud in dashboard

--- librefit-web/src/routes/+page.server.js
+++ librefit-web/src/routes/+page.server.js
@@ -1,6 +1,7 @@
 import { api } from '$lib/server/api/index.js';
 import { proxyFetch } from '$lib/server/api/util.js';
 import { getDateAsStr } from '$lib/util.js';
+import { Category } from '$lib/api/model.js';
 
 /** @type {import('./$types').PageServerLoad} */
 export const load = async ({ fetch, cookies }) => {
@@ -25,13 +26,27 @@ export const load = async ({ fetch, cookies }) => {
 	const lastWeightResponse = await proxyFetch(fetch, lastWeightApi, jwt);
 	const lastGoalResponse = await proxyFetch(fetch, goalApi, jwt);
 	const lastCtResponse = await proxyFetch(fetch, ctTodayApi, jwt, { date: getDateAsStr(today) });
+
+	/** @type {Array<CalorieTrackerEntry>} */
+	const ctList = await lastCtResponse.json();
+
+	// add a blank entry for new input
+	/** @type {CalorieTrackerEntry} */
+	const blankEntry = {
+		added: getDateAsStr(today),
+		amount: 0,
+		category: Category.Unset
+	};
+
+	ctList.unshift(blankEntry);
+
 	const userData = await proxyFetch(fetch, userApi, jwt);
 
 	return {
 		authenticated: true,
 		lastWeight: await lastWeightResponse.json(),
 		lastGoal: await lastGoalResponse.json(),
-		lastCt: await lastCtResponse.json(),
+		lastCt: ctList,
 		user: await userData.json()
 	};
 };
--- librefit-web/src/routes/+page.svelte
+++ librefit-web/src/routes/+page.svelte
@@ -2,8 +2,56 @@
 	import WeightTracker from '$lib/components/tracker/WeightTracker.svelte';
 	import {categoriesAsKeyValue, getDaytimeGreeting} from '$lib/util.js';
 	import CalorieTracker from '$lib/components/tracker/CalorieTracker.svelte';
+	import {handleApiError, showToastSuccess} from '$lib/toast.js';
+	import {getToastStore} from '@skeletonlabs/skeleton';
+	import * as ct_crud from '$lib/api/calories-rest.js';
 
 	export let data;
+
+	/** @type {Array<CalorieTrackerEntry>} */
+	let calorieTrackerEntries = [];
+
+	$: calorieTrackerEntries;
+	$: if (data && data.lastCt) {
+		calorieTrackerEntries = data.lastCt;
+	}
+
+	const toastStore = getToastStore();
+
+	const addCalories = (e) => {
+		const id = Math.max(...data.lastCt.map(entry => entry.id).filter(id => id !== undefined)) + 1;
+
+		ct_crud.addEntry(e, loadCalorieTrackerEntries, toastStore, '/', id);
+	};
+
+	const updateCalories = (e) => {
+		ct_crud.updateEntry(e, loadCalorieTrackerEntries, toastStore, '/');
+	};
+
+	const deleteCalories = (e) => {
+		ct_crud.deleteEntry(e, loadCalorieTrackerEntries, toastStore, '/', 'type=ct');
+	};
+
+	const setWeight = (e) => {
+
+	};
+
+	const setGoal = (e) => {
+
+	};
+
+	const loadCalorieTrackerEntries = async (added) => {
+		const response = await fetch(`/?type=ct&added=${added}`, {method: 'GET'});
+		const result = response.json();
+
+		calorieTrackerEntries = await result;
+
+		if (response.ok) {
+			return result;
+		} else {
+			throw new Error(result);
+		}
+	}
 </script>
 
 <section>
@@ -15,10 +63,17 @@
 
 		<div class="flex flex-row gap-8">
 			<div class="flex flex-row gap-4 variant-ghost-surface rounded-xl p-4">
-				<CalorieTracker entries={data.lastCt} categories={categoriesAsKeyValue} />
+				<CalorieTracker entries={calorieTrackerEntries} categories={categoriesAsKeyValue}
+					on:addCalories={addCalories}
+					on:updateCalories={updateCalories}
+					on:deleteCalories={deleteCalories}
+				/>
 			</div>
 			<div class="variant-ghost-surface rounded-xl p-4">
-				<WeightTracker entries={[]} lastEntry={data.lastWeight} goal={data.lastGoal}/>
+				<WeightTracker entries={[]} lastEntry={data.lastWeight} goal={data.lastGoal}
+					on:updateWeight={setWeight}
+				   	on:updateGoal={setGoal}
+				/>
 			</div>
 		</div>
 	</div>
