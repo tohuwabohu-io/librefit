c21773c2645b3d12153c322f79728bab812ad6de
Stefan Poindl <stefan@tohuwabohu.io>
2023-11-04 23:54:18 +0100

    move weight tracker client side crud to own file

---
+++ librefit-web/src/lib/api/weight-rest.js
@@ -0,0 +1,69 @@
+import { handleApiError } from '$lib/toast.js';
+
+/**
+ * @param e
+ * @param {Function} callback
+ * @param {import('@skeletonlabs/skeleton').ToastStore} toastStore
+ * @param {String} route
+ */
+export const add = (e, callback, toastStore, route) => {
+	fetch(route, {
+		method: 'POST',
+		body: JSON.stringify({
+			weight: {
+				id: e.detail.sequence,
+				added: e.detail.todayDateStr,
+				amount: e.detail.value
+			}
+		}),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	})
+		.then(callback)
+		.catch((e) => handleApiError(toastStore, e));
+};
+
+/**
+ * @param e
+ * @param {Function} callback
+ * @param {import('@skeletonlabs/skeleton').ToastStore} toastStore
+ * @param {String} route
+ */
+export const update = (e, callback, toastStore, route) => {
+	fetch(route, {
+		method: 'PUT',
+		body: JSON.stringify({
+			weight: {
+				id: e.detail.sequence,
+				date: e.detail.date,
+				amount: e.detail.value
+			}
+		})
+	})
+		.then(callback)
+		.catch((e) => handleApiError(toastStore, e));
+};
+
+/**
+ * @param e
+ * @param {Function} callback
+ * @param {import('@skeletonlabs/skeleton').ToastStore} toastStore
+ * @param {String} route
+ * @param {String} [params]
+ */
+export const remove = (e, callback, toastStore, route, params) => {
+	let query = route;
+
+	if (params === undefined) {
+		query += `?sequence=${e.detail.sequence}&added=${e.detail.date}`;
+	} else {
+		query += `?${params}&sequence=${e.detail.sequence}&added=${e.detail.date}`;
+	}
+
+	fetch(query, {
+		method: 'DELETE'
+	})
+		.then(callback)
+		.catch((e) => handleApiError(toastStore, e));
+};
