259ed022b79fa0b7ac638c7b58135e48856a783a
Stefan Poindl <stefan@tohuwabohu.io>
2023-11-09 19:09:02 +0100

    remove table creation SQL in favor of hiernate ORM; create user data on startup

---
+++ librefit-service/src/main/kotlin/io/tohuwabohu/security/Startup.kt
@@ -0,0 +1,44 @@
+package io.tohuwabohu.security
+
+import io.quarkus.runtime.StartupEvent
+import io.quarkus.vertx.VertxContextSupport
+import io.tohuwabohu.crud.LibreUser
+import jakarta.enterprise.event.Observes
+import jakarta.inject.Singleton
+import org.hibernate.reactive.mutiny.Mutiny
+
+
+@Singleton
+class Startup {
+
+    fun loadUsers(@Observes event: StartupEvent, sf: Mutiny.SessionFactory) {
+
+        VertxContextSupport.subscribeAndAwait {
+            sf.withTransaction { session ->
+                session.persistAll(
+                    LibreUser(
+                        0L,
+                        "test1@test.dev",
+                        "test1",
+                        "User",
+                        "Testuser 1"
+                    ),
+                    LibreUser(
+                        0L,
+                        "test2@test.dev",
+                        "test2",
+                        "User",
+                        "Testuser 2"
+                    ),
+                    LibreUser(
+                        0L,
+                        "test3@test.dev",
+                        "test3",
+                        "User",
+                        "Testuser 3"
+                    )
+                )
+            }
+        }
+    }
+}
--- librefit-service/src/main/resources/import.sql
+++ librefit-service/src/main/resources/import.sql
@@ -1,11 +1,3 @@
-insert into libre_user (id, email, name, password, registered)
-values (1, 'test1@test.dev', 'test1', 'test1', '2023-03-01'),
-       (2, 'test2@test.dev', 'Johnny Cool', 'test2', '2023-03-01'),
-       (3, 'test3@test.dev', 'Hugh Jazz', 'test3', '2023-03-01'),
-       (4, 'test4@test.dev', 'Donald Duck', 'test4', '2023-03-01'),
-       (5, 'test5@test.dev', 'Gustav Gans', 'test5', '2023-03-01'),
-       (6, 'test6@test.dev', 'Darkwing Duck', 'test6', '2023-03-01');
-
 insert into calorie_tracker_entry (added, id, user_id, amount, category)
 values ('2023-02-01', 1, 1, 240, 'b'),
        ('2023-02-01', 2, 1, 270, 'l'),
