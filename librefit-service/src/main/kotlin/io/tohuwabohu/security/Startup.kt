707e81a996d4ec72dd27a1b2d3273ef22334ae0c
Stefan Poindl <stefan@tohuwabohu.io>
2023-11-09 19:38:46 +0100

    add users on startup with dev profile only

--- librefit-service/src/main/kotlin/io/tohuwabohu/security/Startup.kt
+++ librefit-service/src/main/kotlin/io/tohuwabohu/security/Startup.kt
@@ -1,6 +1,7 @@
 package io.tohuwabohu.security
 
 import io.quarkus.runtime.StartupEvent
+import io.quarkus.runtime.configuration.ConfigUtils
 import io.quarkus.vertx.VertxContextSupport
 import io.tohuwabohu.crud.LibreUser
 import jakarta.enterprise.event.Observes
@@ -10,34 +11,34 @@ import org.hibernate.reactive.mutiny.Mutiny
 
 @Singleton
 class Startup {
-
     fun loadUsers(@Observes event: StartupEvent, sf: Mutiny.SessionFactory) {
-
-        VertxContextSupport.subscribeAndAwait {
-            sf.withTransaction { session ->
-                session.persistAll(
-                    LibreUser(
-                        0L,
-                        "test1@test.dev",
-                        "test1",
-                        "User",
-                        "Testuser 1"
-                    ),
-                    LibreUser(
-                        0L,
-                        "test2@test.dev",
-                        "test2",
-                        "User",
-                        "Testuser 2"
-                    ),
-                    LibreUser(
-                        0L,
-                        "test3@test.dev",
-                        "test3",
-                        "User",
-                        "Testuser 3"
+        if (ConfigUtils.getProfiles().contains("dev")) {
+            VertxContextSupport.subscribeAndAwait {
+                sf.withTransaction { session ->
+                    session.persistAll(
+                        LibreUser(
+                            0L,
+                            "test1@test.dev",
+                            "test1",
+                            "User",
+                            "Testuser 1"
+                        ),
+                        LibreUser(
+                            0L,
+                            "test2@test.dev",
+                            "test2",
+                            "User",
+                            "Testuser 2"
+                        ),
+                        LibreUser(
+                            0L,
+                            "test3@test.dev",
+                            "test3",
+                            "User",
+                            "Testuser 3"
+                        )
                     )
-                )
+                }
             }
         }
     }
