6910581b230ce7f2fdbc61f7a80b9f9599ffc809
Stefan Poindl <stefan@tohuwabohu.io>
2023-11-13 19:20:02 +0100

    add custom error page with an awesome image

---
+++ librefit-web/src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+    import {page} from '$app/stores';
+    import ErrorBot from '$lib/assets/icons/error-bot.svg?component';
+</script>
+
+<section>
+    <div class="container mx-auto p-8 space-y-8">
+        <h1>{$page.status}: {$page.error.message}</h1>
+        <p>Whoops. Seems like that took a wrong turn.</p>
+        <div class="flex justify-center">
+            <ErrorBot height="60vh"/>
+        </div>
+    </div>
+</section>
