2a40d0dacd0b5d3a741ac13620055d0541614665
Stefan Poindl <stefan@tohuwabohu.io>
2023-11-15 10:38:59 +0100

    fix payload in wizard +server

--- librefit-web/src/routes/wizard/+server.js
+++ librefit-web/src/routes/wizard/+server.js
@@ -7,21 +7,21 @@ import * as weight_crud from '$lib/server/api/weight-rest.js';
  */
 export async function POST({ request, fetch, cookies }) {
 	const peek = request.clone();
-	const payload = await peek.json();
+	const peeked = await peek.json();
 
 	/** @type Response */
 	let response;
 
-	if (payload.tdee) {
-		const tdee = await request.json();
+	if (peeked.tdee) {
+		const payload = await request.json();
 		const tdeeApi = api.calculateTdee;
 
 		/** @type {Response} */
-		response = await proxyFetch(fetch, tdeeApi, undefined, tdee);
-	} else if (payload.goal) {
+		response = await proxyFetch(fetch, tdeeApi, undefined, payload.tdee);
+	} else if (peeked.goal) {
 		response = weight_crud.POST({ request, fetch, cookies });
 	} else {
-		console.error(`wizard api can't be called with payload ${JSON.stringify(payload)}`);
+		console.error(`wizard api can't be called with payload ${JSON.stringify(peeked)}`);
 
 		response = new Response(null, {
 			status: 422,
