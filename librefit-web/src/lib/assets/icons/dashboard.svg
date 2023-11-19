fa5ae5927045b04bfa723ed446c98622d93c4ec4
Stefan Poindl <stefan@tohuwabohu.io>
2023-11-19 21:17:41 +0100

    add some icons for user panel

---
+++ librefit-web/src/lib/assets/icons/dashboard.svg
@@ -0,0 +1,1 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4h6v8H4zm0 12h6v4H4zm10-4h6v8h-6zm0-8h6v4h-6z"/></svg>
--- librefit-web/src/lib/components/UserPanel.svelte
+++ librefit-web/src/lib/components/UserPanel.svelte
@@ -3,6 +3,12 @@
     import {Avatar, getDrawerStore} from '@skeletonlabs/skeleton';
     import {goto} from '$app/navigation';
     import {createEventDispatcher, getContext} from 'svelte';
+    import Dashboard from '$lib/assets/icons/dashboard.svg?component';
+    import Wand from '$lib/assets/icons/wand.svg?component';
+    import User from '$lib/assets/icons/user.svg?component';
+    import GitHub from '$lib/assets/icons/github.svg?component';
+    import Food from '$lib/assets/icons/food.svg?component';
+    import Scale from '$lib/assets/icons/scale-outline.svg?component'
 
     const drawerStore = getDrawerStore();
 
@@ -41,32 +47,50 @@
         <ul>
             <li>
                 <a href="/dashboard" class="{classesActive('/dashboard')}" on:click={() => drawerStore.close()}>
-                    Dashboard
+                    <span><Dashboard/></span>
+                    <span class="flex-auto">
+                        Dashboard
+                    </span>
                 </a>
             </li>
             <li>
                 <a href="/profile" class="{classesActive('/profile')}" on:click={() => drawerStore.close()}>
-                    Profile
+                    <span><User/></span>
+                    <span class="flex-auto">
+                        Profile
+                    </span>
                 </a>
             </li>
             <li>
                 <a href="/wizard" class="{classesActive('/wizard')}" on:click={() => drawerStore.close()}>
-                    Wizard
+                    <span><Wand/></span>
+                    <span class="flex-auto">
+                        Wizard
+                    </span>
                 </a>
             </li>
             <li>
                 <a href="/tracker/calories" class="{classesActive('/tracker/calories')}" on:click={() => drawerStore.close()}>
-                    Calorie Tracker
+                    <span><Food/></span>
+                    <span class="flex-auto">
+                        Calorie Tracker
+                    </span>
                 </a>
             </li>
             <li>
                 <a href="/tracker/weight" class="{classesActive('/tracker/weight')}" on:click={() => drawerStore.close()}>
-                    Weight Tracker
+                    <span><Scale/></span>
+                    <span class="flex-auto">
+                        Weight Tracker
+                    </span>
                 </a>
             </li>
             <li>
                 <a href="https://github.com/tohuwabohu-io/librefit" target="_blank">
-                    GitHub
+                    <span><GitHub/></span>
+                    <span class="flex-auto">
+                        GitHub
+                    </span>
                 </a>
             </li>
         </ul>
