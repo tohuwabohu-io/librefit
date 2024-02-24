100990d5d0a84fe7639c0a6e415e8d6121413f40
Stefan Poindl <stefan@tohuwabohu.io>
2024-02-24 18:56:21 +0100

    move registration business logic to register.js

---
+++ librefit-web/src/lib/components/modal/RegistrationModal.svelte
@@ -0,0 +1,23 @@
+<script>
+    import {getModalStore} from '@skeletonlabs/skeleton';
+
+    let value;
+
+    const modalStore = getModalStore();
+
+    const onSubmit = () => {
+        if ($modalStore[0].response) {
+            $modalStore[0].response({
+                detail: {
+                    value: value
+                }
+            });
+        }
+    }
+
+    const onCancel = () => {
+        if ($modalStore[0].response) {
+            $modalStore[0].response(undefined);
+        }
+    }
+</script>
