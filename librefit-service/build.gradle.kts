d579ee5b1bdee265bbd6f7df6dbffb30243892c8
Stefan Poindl <stefan@tohuwabohu.io>
2024-02-28 20:44:23 +0100

    copy *.pem to target folder

--- librefit-service/build.gradle.kts
+++ librefit-service/build.gradle.kts
@@ -71,15 +71,6 @@ tasks.register("generateKeypair") {
     }
 }
 
-
-tasks.test {
-    dependsOn("generateKeypair")
-}
-
 tasks.testClasses {
-    dependsOn("generateKeypair")
-}
-
-tasks.testNative {
     dependsOn("generateKeypair")
 }
--- librefit-service/generate-keypair.sh
+++ librefit-service/generate-keypair.sh
@@ -1,12 +1,11 @@
 echo "Generating keypair..."
 openssl req -newkey rsa:2048 -new -nodes -keyout privateKey.pem -out csr.pem 2>/dev/null
 openssl rsa -in privateKey.pem -pubout > publicKey.pem 2>/dev/null
+cp privateKey.pem build/resources/main/
+cp publicKey.pem build/resources/main/
 mv privateKey.pem src/main/resources/
 mv publicKey.pem src/main/resources/
 x=$(ls -l src/main/resources/*.pem | wc -l)
-ls -l src/main/resources/*.pem
-chmod a+r privateKey.pem publicKey.pem
-ls -l src/main/resources/*.pem
 echo "... finished. Counting $x generated files."
 
 if [ "$x" -lt 2 ]
