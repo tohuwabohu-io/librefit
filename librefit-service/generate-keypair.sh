11108481f4ae769b22a76813c568385b8227eaae
Stefan Poindl <stefan@tohuwabohu.io>
2024-02-28 19:04:03 +0100

    make keypair generation script failsafe... ish

---
+++ librefit-service/generate-keypair.sh
@@ -0,0 +1,13 @@
+echo "Generating keypair..."
+openssl req -newkey rsa:2048 -new -nodes -keyout privateKey.pem -out csr.pem 2>/dev/null
+openssl rsa -in privateKey.pem -pubout > publicKey.pem 2>/dev/null
+mv privateKey.pem src/main/resources/
+mv publicKey.pem src/main/resources/
+x=$(ls -l src/main/resources/*.pem | wc -l)
+echo "... finished. Counting $x generated files."
+
+if [ "$x" -lt 2 ]
+  then
+    echo "Less than expected files created."
+    exit 1
+fi
