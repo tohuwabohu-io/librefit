3b75bcc78d84dacf79a5d1f30e54021578894293
Stefan Poindl <55439476+tohuwabohu-io@users.noreply.github.com>
2024-03-07 10:34:47 +0100

    67 tdee calculator test (#68)
    refactor tdee calculator; add tests

--- librefit-service/src/main/kotlin/io/tohuwabohu/TdeeResource.kt
+++ librefit-service/src/main/kotlin/io/tohuwabohu/TdeeResource.kt
@@ -1,12 +1,18 @@
 package io.tohuwabohu
 
+import io.quarkus.logging.Log
+import io.smallrye.mutiny.Uni
 import io.tohuwabohu.calc.CalculationGoal
 import io.tohuwabohu.calc.CalculationSex
 import io.tohuwabohu.calc.Tdee
+import io.tohuwabohu.calc.TdeeCalculator
+import io.tohuwabohu.crud.error.ErrorResponse
+import io.tohuwabohu.crud.error.createErrorResponse
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
 import org.eclipse.microprofile.openapi.annotations.Operation
 import org.eclipse.microprofile.openapi.annotations.media.Content
 import org.eclipse.microprofile.openapi.annotations.media.Schema
@@ -14,7 +20,7 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponse
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses
 
 @Path("/tdee")
-class TdeeResource {
+class TdeeResource(private val calculator: TdeeCalculator) {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -24,6 +30,12 @@ class TdeeResource {
             Content(
                 mediaType = "application/json",
                 schema = Schema(implementation = Tdee::class)
+            )
+        ]),
+        APIResponse(responseCode = "400", description = "Bad Request", content = [
+            Content(
+                mediaType = "application/json",
+                schema = Schema(implementation = ErrorResponse::class)
             )
         ]),
         APIResponse(responseCode = "500", description = "Internal Server Error")
@@ -37,5 +49,10 @@ class TdeeResource {
         activityLevel: Float,
         weeklyDifference: Float,
         calculationGoal: CalculationGoal
-    ) = Tdee(age, sex, weight, height, activityLevel, weeklyDifference, calculationGoal)
+    ): Uni<Response> {
+        return calculator.calculate(Tdee(age, sex, weight, height, activityLevel, weeklyDifference, calculationGoal))
+            .onItem().transform { result -> Response.ok(result).build() }
+            .onFailure().invoke { e -> Log.error(e) }
+            .onFailure().recoverWithItem{ throwable -> createErrorResponse(throwable) }
+    }
 }
