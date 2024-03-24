74445dd2bd45b5324c73217290febb0ca0fa3057
Stefan Poindl <stefan@tohuwabohu.io>
2024-03-24 19:58:26 +0100

    remove imprint page

--- librefit-web/src/routes/imprint/+page.svelte
+++
@@ -1,7 +0,0 @@
-<section class="variant-ghost-surface h-full flex">
-    <div class="container mx-auto p-12 space-y-8 self-center">
-        <h1>
-            Imprint
-        </h1>
-    </div>
-</section>
