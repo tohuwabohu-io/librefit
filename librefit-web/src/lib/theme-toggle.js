c8a34efad944e2103641fd62999f919435fe2643
Stefan Poindl <stefan@tohuwabohu.io>
2024-05-03 22:29:53 +0200

    refresh calorie distribution chart when dark theme toggle was clicked

---
+++ librefit-web/src/lib/theme-toggle.js
@@ -0,0 +1,11 @@
+export const observeToggle = (element, callback) => {
+	const observer = new MutationObserver((mutations) => {
+		mutations.forEach((mutation) => {
+			if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+				callback(mutation.target);
+			}
+		});
+	});
+	observer.observe(element, { attributes: true });
+	return observer.disconnect;
+};
