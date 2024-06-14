beec8e07c0e41769fa9eea3387e00cfd0f43d949
Stefan Poindl <stefan@tohuwabohu.io>
2024-06-14 21:41:58 +0200

    bump kotlin version 1.9.0 -> 2.0.0

--- librefit-service/build.gradle.kts
+++ librefit-service/build.gradle.kts
@@ -1,8 +1,10 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    kotlin("jvm") version "1.9.0"
-    kotlin("plugin.allopen") version "1.9.0"
-    kotlin("plugin.noarg") version "1.9.0"
-    kotlin("plugin.jpa") version "1.9.0"
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.allopen") version "2.0.0"
+    kotlin("plugin.noarg") version "2.0.0"
+    kotlin("plugin.jpa") version "2.0.0"
     id("io.quarkus")
 }
 
@@ -16,7 +18,7 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
-    implementation("io.quarkiverse.quinoa:quarkus-quinoa:2.3.5")
+    implementation("io.quarkiverse.quinoa:quarkus-quinoa:2.3.6")
     implementation("io.quarkus:quarkus-jdbc-postgresql")
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-flyway")
@@ -43,11 +45,6 @@ dependencies {
 
 group = "io.tohuwabohu"
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-
 tasks.withType<Test> {
     systemProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager")
 }
@@ -61,8 +58,10 @@ allOpen {
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
-    kotlinOptions.javaParameters = true
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+        javaParameters = true
+    }
 }
 
 tasks.register("generateKeypair") {
