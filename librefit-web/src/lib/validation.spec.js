08b599935e4480a296a91fe1e3394a1d7bb549c9
Stefan Poindl <stefan@tohuwabohu.io>
2024-06-25 08:16:00 +0200

    add test for frontend validation

---
+++ librefit-web/src/lib/validation.spec.js
@@ -0,0 +1,64 @@
+// Importing the required dependencies
+import { expect, describe, it } from 'vitest';
+import { validateFields } from './validation';
+
+describe('validateFields', () => {
+	it('should return undefined when all fields are valid', () => {
+		const fields = {
+			email: 'test@test.com',
+			password: 'Password@123',
+			passwordConfirmation: 'Password@123',
+			confirmation: true
+		};
+		const result = validateFields(fields);
+		expect(result).toBeUndefined();
+	});
+
+	it('should return errors when email is not valid', () => {
+		const fields = {
+			email: 'test@test',
+			password: 'Password@123',
+			passwordConfirmation: 'Password@123',
+			confirmation: true
+		};
+		const result = validateFields(fields);
+		expect(result.errors).toBeTruthy();
+		expect(result.errors[0].field).toBe('email');
+	});
+
+	it('should return errors when password is not valid', () => {
+		const fields = {
+			email: 'test@test.com',
+			password: 'Pass',
+			passwordConfirmation: 'Pass',
+			confirmation: true
+		};
+		const result = validateFields(fields);
+		expect(result.errors).toBeTruthy();
+		expect(result.errors[0].field).toBe('password');
+	});
+
+	it('should return errors when password confirmation is not valid', () => {
+		const fields = {
+			email: 'test@test.com',
+			password: 'Password@123',
+			passwordConfirmation: 'Password@124',
+			confirmation: true
+		};
+		const result = validateFields(fields);
+		expect(result.errors).toBeTruthy();
+		expect(result.errors[0].field).toBe('passwordConfirmation');
+	});
+
+	it('should return errors when tos is not accepted', () => {
+		const fields = {
+			email: 'test@test.com',
+			password: 'Password@123',
+			passwordConfirmation: 'Password@123',
+			confirmation: false
+		};
+		const result = validateFields(fields);
+		expect(result.errors).toBeTruthy();
+		expect(result.errors[0].field).toBe('confirmation');
+	});
+});
