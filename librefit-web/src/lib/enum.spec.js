eaf508481627f209f71e6587f142558fac131928
Stefan Poindl <stefan@tohuwabohu.io>
2024-06-25 08:51:23 +0200

    add test for enum functions

---
+++ librefit-web/src/lib/enum.spec.js
@@ -0,0 +1,23 @@
+import { describe, expect, it } from 'vitest';
+import { DataViews, enumKeys } from './enum';
+
+describe('Enum', () => {
+	it('should correctly define DataViews', () => {
+		expect(DataViews.Week).toBe('WEEK');
+		expect(DataViews.Month).toBe('MONTH');
+		expect(DataViews.Year).toBe('YEAR');
+	});
+
+	it('should correctly define enumKeys', () => {
+		let obj = {
+			a: 1,
+			b: 2,
+			3: 'c'
+		};
+		let keys = enumKeys(obj);
+
+		expect(keys).toContain('a');
+		expect(keys).toContain('b');
+		expect(keys).not.toContain('3');
+	});
+});
