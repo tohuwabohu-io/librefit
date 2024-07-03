b29ea24dbe6311cecf586aefbf079031db8c4048
Stefan Poindl <stefan@tohuwabohu.io>
2024-07-03 12:24:35 +0200

    add root build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm") apply false
+    kotlin("plugin.serialization") apply false
+    kotlin("plugin.allopen") apply false
+    kotlin("plugin.noarg") apply false
+    kotlin("plugin.jpa") apply false
+
+    id("io.quarkus") apply false
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
+    apply(plugin = "org.jetbrains.kotlin.plugin.allopen")
+    apply(plugin = "org.jetbrains.kotlin.plugin.noarg")
+    apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
+
+    apply(plugin = "io.quarkus")
+}
