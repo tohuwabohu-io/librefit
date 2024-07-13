43bcc3bcbd65e836de02bd3120c03cbaee190507
Stefan Poindl <stefan@tohuwabohu.io>
2024-07-13 10:18:05 +0200

    update flyway migrations

--- librefit-service/src/main/resources/db/migration/V2024.08.01__authentication_rework.sql
+++ librefit-service/src/main/resources/db/migration/V2024.08.01__authentication_rework.sql
@@ -1,1 +1,24 @@
 drop table auth_session;
+
+drop table goal;
+
+create table calorie_tracker_entry (
+  added date not null,
+  amount real not null,
+  sequence bigint not null,
+  updated timestamp(6) without time zone,
+  user_id uuid not null,
+  category character varying(255) not null,
+  description character varying(255),
+  primary key (added, sequence, user_id)
+);
+
+create table weight_tracker_entry (
+     added date not null,
+     amount real not null,
+     sequence bigint not null,
+     updated timestamp(6) without time zone,
+     user_id uuid not null,
+     primary key (added, sequence, user_id)
+);
+
