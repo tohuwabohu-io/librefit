9a9a005cd59e66da2eb8d62f1f3013a11d2a5f33
Stefan Poindl <stefan@tohuwabohu.io>
2024-07-18 09:09:27 +0200

    fix unit test sql

--- librefit-service/src/test/resources/import.sql
+++ librefit-service/src/test/resources/import.sql
@@ -13,5 +13,5 @@ values ('b', 'Breakfast', true),
 insert into libre_user (id, email, password, role, name, avatar, activated) values ('2291b08c-7fb5-11ee-b962-0242ac120002', 'dashboard@test.dev', '<pwd>', 'User', 'Sly', '/assets/rambo-1.jpg', true);
 insert into calorie_tracker_entry (user_id, added, sequence, category, amount) values ('2291b08c-7fb5-11ee-b962-0242ac120002', '2024-05-31', 1, 'l', 300);
 insert into weight_tracker_entry (user_id, added, sequence, amount) values ('2291b08c-7fb5-11ee-b962-0242ac120002', '2024-05-31', 1, 80);
-insert into calorie_target(user_id, added, sequence, start_date, end_date, target_calories) values ('2291b08c-7fb5-11ee-b962-0242ac120002', '2024-05-31', 1, '2024-01-01', '2024-12-31', 1900);
+insert into calorie_target(user_id, added, sequence, start_date, end_date, target_calories, maximum_calories) values ('2291b08c-7fb5-11ee-b962-0242ac120002', '2024-05-31', 1, '2024-01-01', '2024-12-31', 1900, 2400);
 insert into weight_target(user_id, added, sequence, start_date, end_date, initial_weight, target_weight) values ('2291b08c-7fb5-11ee-b962-0242ac120002', '2024-05-01', 1, '2024-01-01', '2024-12-31', 90, 75);
