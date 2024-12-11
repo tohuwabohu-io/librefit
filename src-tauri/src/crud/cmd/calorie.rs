4a7c8f19016a1343e5478a86e4843a3ab3500576
Stefan Poindl <stefan@tohuwabohu.io>
2024-12-11 15:39:05 +0100

    remove dead code

--- src-tauri/src/crud/cmd/calorie.rs
+++ src-tauri/src/crud/cmd/calorie.rs
@@ -1,38 +1,14 @@
+use crate::crud::cmd::error_handler::handle_error;
 use crate::crud::db::connection::create_db_connection;
 use crate::crud::db::model::{CalorieTarget, CalorieTracker, NewCalorieTarget, NewCalorieTracker};
 use crate::crud::db::repo::calories;
 use tauri::command;
-use crate::crud::cmd::error_handler::handle_error;
 
 /// Create a new calorie target
 #[command]
 pub fn create_calorie_target(new_target: NewCalorieTarget) -> Result<CalorieTarget, String> {
     let conn = &mut create_db_connection(); // Your function to create database connections
     calories::create_calorie_target(conn, &new_target).map_err(handle_error)
-}
-
-/// Retrieve all calorie targets
-#[command]
-fn get_calorie_targets() -> Result<Vec<CalorieTarget>, String> {
-    let conn = &mut create_db_connection();
-    calories::get_calorie_targets(conn).map_err(handle_error)
-}
-
-/// Update a calorie target by ID
-#[command]
-fn update_calorie_target(
-    target_id: i32,
-    updated_target: NewCalorieTarget,
-) -> Result<CalorieTarget, String> {
-    let conn = &mut create_db_connection();
-    calories::update_calorie_target(conn, target_id, updated_target).map_err(handle_error)
-}
-
-/// Delete a calorie target by ID
-#[command]
-fn delete_calorie_target(target_id: i32) -> Result<usize, String> {
-    let conn = &mut create_db_connection();
-    calories::delete_calorie_target(conn, target_id).map_err(handle_error)
 }
 
 /// Create a new calorie tracker entry and return that day's tracker data
@@ -72,18 +48,19 @@ pub fn delete_calorie_tracker_entry(
 #[command]
 pub fn get_calorie_tracker_for_date_range(
     date_from_str: String,
-    date_to_str: String
+    date_to_str: String,
 ) -> Result<Vec<CalorieTracker>, String> {
     let conn = &mut create_db_connection();
 
-    calories::find_calorie_tracker_by_date_range(conn, &date_from_str, &date_to_str).map_err(handle_error)
+    calories::find_calorie_tracker_by_date_range(conn, &date_from_str, &date_to_str)
+        .map_err(handle_error)
 }
 
 /// Return all dates the user has actually tracked something in the given range.
 #[command]
 pub fn get_calorie_tracker_dates_in_range(
     date_from_str: String,
-    date_to_str: String
+    date_to_str: String,
 ) -> Result<Vec<String>, String> {
     let conn = &mut create_db_connection();
 
